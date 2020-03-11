1db30a24889745658ef81eb918fa9e9d2b0b9c7f
Safa Fredj <fredj.safa@gmail.com>
2020-03-11 11:37:25 +0100

    chore:json5 add ocnfig webpack

--- src/js/hello.js
+++ src/js/hello.js
@@ -1,4 +1,6 @@
+// const { config } = require('json5-loader!./config.json5');
+var config = require('./config.json5');
 function hello(name) {
-  return `Hello ${name}`;
+  return `${config.message} ${name}`;
 }
 export { hello };
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,12 @@ const config = {
           'sass-loader',
         ],
       },
+      {
+        // make all files ending in .json5 use the `json5-loader`
+        test: /\.json5$/,
+        use: 'json5-loader',
+        type: 'javascript/auto',
+      },
     ],
   },
 };
